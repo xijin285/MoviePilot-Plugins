3e529a9bdc2b83e489f67a4470b4bd654304bf80
xiaodo168 <xijin858@gmail.com>
2025-05-20 04:47:58 +0800

    测试

--- plugins.v2/randompic/__init__.py
+++ plugins.v2/randompic/__init__.py
@@ -16,7 +16,7 @@ class RandomPicPlugin(PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/madrays/MoviePilot-Plugins/main/icons/random.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "jinxi"
     # 作者主页
