ab66ef82defda9d732964d6d30978069630ea6c5
xiaodo168 <xijin858@gmail.com>
2025-05-20 03:05:37 +0800

    测试

--- plugins.v2/siteloginmonitor/__init__.py
+++ plugins.v2/siteloginmonitor/__init__.py
@@ -23,7 +23,7 @@ class SiteLoginMonitor(PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/xijin285/MoviePilot-Plugins/refs/heads/main/icons/sitemonitor.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "jinxi"
     # 作者主页
